8836a81368fc070e82ba1e59d5a05940b6807255
amd5200 <amd5200killer@gmail.com>
2019-04-20 19:29:21 +0800

    Update autotracking.sh

--- security-cam-auto-track/autotracking.sh
+++ security-cam-auto-track/autotracking.sh
@@ -9,6 +9,7 @@
 sudo chmod 777 /dev/ttyS0
 
 #python3 security-cam-serial-fixedcam.py --video 0 & python3 key_control_py3.py
+#python3 security-cam-rpi3B-serial-movecam.py --video 0 --graph face_graph --labels face_labels.txt & python3 key_control_py3.py
 python3 security-cam-rpi3B-serial-movecam.py & python3 key_control_py3.py
 
 read -p "press Enter to exit" a
